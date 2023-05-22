1d04618f58dfec2bc3369019dc4192e93442f5a8
Derek Ryan Strong <dstrong@usc.edu>
2023-05-22 11:38:34 -0700

    Update ior-scratch1.py

--- tests/ior-scratch1.py
+++ tests/ior-scratch1.py
@@ -3,32 +3,38 @@ import reframe.utility.sanity as sn
 
 @rfm.simple_test
 class IOR_scratch1(rfm.RunOnlyRegressionTest):
-    def __init__(self):
-        self.descr = 'IOR benchmark for /scratch1 file system'
-        self.tags = {
-            'daily'
+    descr = 'IOR benchmark for /scratch1 file system'
+    tags = {
+        'daily'
+    }
+    valid_systems = [
+        'discovery:epyc-64',
+        'endeavour:shared'
+    ]
+    valid_prog_environs = [
+        'PrgEnv-ior'
+    ]
+    sourcesdir = None
+    executable = 'ior -vvv -t 4m -b 64m -s 16 -F -C -e -o /scratch1/$USER/reframe.tmp'
+    num_tasks = 16
+    num_tasks_per_node = 4
+    num_cpus_per_task = 1
+    time_limit = '5m'
+    reference = {
+        '*': {
+            'max_write_speed': (3000.00, -0.25, None, 'MiB/sec'),
+            'max_read_speed': (15000.00, -0.25, None, 'MiB/sec')
         }
-        self.valid_systems = [
-            'discovery:epyc-64',
-            'endeavour:shared'
-        ]
-        self.valid_prog_environs = [
-            'PrgEnv-ior'
-        ]
-        self.sourcesdir = None
-        self.executable = 'ior -vvv -t 4m -b 64m -s 16 -F -C -e -o /scratch1/$USER/reframe.tmp'
-        self.num_tasks = 16
-        self.num_tasks_per_node = 4
-        self.num_cpus_per_task = 1
-        self.time_limit = '5m'
-        self.sanity_patterns = sn.assert_found(r'Finished', self.stdout)
-        self.perf_patterns = {
-            'max write speed': sn.extractsingle(r'Max Write:\s+(?P<W_ret>\S+)', self.stdout, 'W_ret', float),
-            'max read speed': sn.extractsingle(r'Max Read:\s+(?P<R_ret>\S+)', self.stdout, 'R_ret', float)
-        }
-        self.reference = {
-            '*': {
-                'max write speed': (3000.00, -0.25, None, 'MiB/sec'),
-                'max read speed': (15000.00, -0.25, None, 'MiB/sec')
-            }
-        }
+    }
+
+    @sanity_function
+    def assert_sanity(self):
+        return sn.assert_found(r'Finished', self.stdout)
+
+    @performance_function('MiB/sec', perf_key = 'max_write_speed')
+    def extract_perf_write(self):
+        return sn.extractsingle(r'Max Write:\s+(?P<W_ret>\S+)', self.stdout, 'W_ret', float)
+
+    @performance_function('MiB/sec', perf_key = 'max_read_speed')
+    def extract_perf_read(self):
+        return sn.extractsingle(r'Max Read:\s+(?P<R_ret>\S+)', self.stdout, 'R_ret', float)
