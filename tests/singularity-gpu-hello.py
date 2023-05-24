79af743250a92c7078f0264e0ee5df67c33a79d8
Derek Ryan Strong <dstrong@usc.edu>
2023-05-24 16:03:46 -0700

    Update singularity-gpu-hello.py

--- tests/singularity-gpu-hello.py
+++ tests/singularity-gpu-hello.py
@@ -3,24 +3,26 @@ import reframe.utility.sanity as sn
 
 @rfm.simple_test
 class Singularity_GPU_Hello(rfm.RunOnlyRegressionTest):
-    def __init__(self):
-        self.descr = 'Singularity GPU access'
-        self.valid_systems = [
-            'discovery:gpu',
-            'endeavour:isi'
-        ]
-        self.valid_prog_environs = [
-            'PrgEnv-singularity'
-        ]
-        self.sourcesdir = None
-        self.executable = 'singularity exec --nv /spack/singularity/ood/centos7-xfce.sif nvidia-smi'
-        self.num_tasks = 1
-        self.num_cpus_per_task = 1
-        self.time_limit = '5m'
-        self.sanity_patterns = sn.assert_found(r'NVIDIA-SMI', self.stdout)
+    descr = 'Singularity GPU access'
+    valid_systems = [
+        'discovery:gpu',
+        'endeavour:isi'
+    ]
+    valid_prog_environs = [
+        'PrgEnv-singularity'
+    ]
+    sourcesdir = None
+    executable = 'singularity exec --nv /spack/singularity/ood/centos7-xfce.sif nvidia-smi'
+    num_tasks = 1
+    num_cpus_per_task = 1
+    time_limit = '5m'
 
     @run_before('run')
     def set_job_options(self):
         self.job.options += [
             '--gpus-per-task=p100:1'
         ]
+
+    @sanity_function
+    def assert_sanity(self):
+        return sn.assert_found(r'NVIDIA-SMI', self.stdout)
