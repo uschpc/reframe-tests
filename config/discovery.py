382ce177f014c5b9d3f6726b08e0c8cac820af3d
Derek Ryan Strong <dstrong@usc.edu>
2023-05-24 15:54:19 -0700

    Update singularity-hello.py

--- config/discovery.py
+++ config/discovery.py
@@ -69,6 +69,7 @@ site_configuration = {
                         'PrgEnv-R',
                         'PrgEnv-matlab',
                         'PrgEnv-qchem',
+                        'PrgEnv-singularity',
                         'PrgEnv-curl'
                     ]
                 },
@@ -116,6 +117,7 @@ site_configuration = {
                         'PrgEnv-R',
                         'PrgEnv-matlab',
                         'PrgEnv-qchem',
+                        'PrgEnv-singularity',
                         'PrgEnv-curl'
                     ]
                 },
--- tests/singularity-hello.py
+++ tests/singularity-hello.py
@@ -3,20 +3,25 @@ import reframe.utility.sanity as sn
 
 @rfm.simple_test
 class Singularity_Hello(rfm.RunOnlyRegressionTest):
-    def __init__(self):
-        self.descr = 'Singularity hello world'
-        self.valid_systems = [
-            'discovery:main',
-            'discovery:oneweek',
-            'discovery:debug',
-            'endeavour:shared'
-        ]
-        self.valid_prog_environs = [
-            'PrgEnv-singularity'
-        ]
-        self.sourcesdir = None
-        self.executable = 'singularity exec /spack/singularity/ood/centos7-xfce.sif echo \"Hello world\"'
-        self.num_tasks = 1
-        self.num_cpus_per_task = 1
-        self.time_limit = '5m'
-        self.sanity_patterns = sn.assert_found(r'Hello world', self.stdout)
+    descr = 'Singularity hello world'
+    valid_systems = [
+        'discovery:main',
+        'discovery:epyc-64',
+        'discovery:gpu',
+        'discovery:largemem',
+        'discovery:debug',
+        'discovery:oneweek',
+        'endeavour:shared'
+    ]
+    valid_prog_environs = [
+        'PrgEnv-singularity'
+    ]
+    sourcesdir = None
+    executable = 'singularity exec /spack/singularity/ood/centos7-xfce.sif echo \"Hello world\"'
+    num_tasks = 1
+    num_cpus_per_task = 1
+    time_limit = '5m'
+
+    @sanity_function
+    def assert_sanity(self):
+        return sn.assert_found(r'Hello world', self.stdout)
