7db18f703e22e16478d5df9dbe9198dd1fe84745
Derek Ryan Strong <dstrong@usc.edu>
2023-05-25 10:46:04 -0700

    Update npb-bt-mz-openmpi.py

--- tests/npb-bt-mz-openmpi.py
+++ tests/npb-bt-mz-openmpi.py
@@ -3,42 +3,49 @@ import reframe.utility.sanity as sn
 
 @rfm.simple_test
 class NPB_BT_MZ_OpenMPI(rfm.RunOnlyRegressionTest):
-    def __init__(self):
-        self.descr = 'NPB BT-MZ MPI+OpenMP benchmark using gcc/11.3.0 and openmpi/4.1.4 with pmix_v2'
-        self.valid_systems = [
-            'discovery:main',
-            'discovery:epyc-64',
-            'endeavour:shared'
-        ]
-        self.valid_prog_environs = [
-            'PrgEnv-gcc-11.3.0-openmpi-4.1.4'
-        ]
-        self.sourcesdir = None
-        self.executable = '/project/hpcroot/reframe2/resources/NPB/gcc-11.3.0/NPB3.4.2-MZ/NPB3.4-MZ-MPI-openmpi-4.1.4/bin/bt-mz.A.x'
-        self.num_tasks = 4
-        self.num_tasks_per_node = 2
-        self.num_cpus_per_task = 2
-        self.time_limit = '5m'
-        self.prerun_cmds = [
-            'ulimit -s unlimited'
-        ]
-        self.env_vars = {
-            'OMP_NUM_THREADS': '2',
-            'SLURM_MPI_TYPE': 'pmix_v2',
-            'SLURM_CPU_BIND': 'verbose'
+    descr = 'NPB BT-MZ MPI+OpenMP benchmark using gcc/11.3.0 and openmpi/4.1.4 with pmix_v2'
+    valid_systems = [
+        'discovery:main',
+        'discovery:epyc-64',
+        'discovery:gpu',
+        'endeavour:shared'
+    ]
+    valid_prog_environs = [
+        'PrgEnv-gcc-11.3.0-openmpi-4.1.4'
+    ]
+    sourcesdir = None
+    executable = '/project/hpcroot/reframe2/resources/NPB/gcc-11.3.0/NPB3.4.2-MZ/NPB3.4-MZ-MPI-openmpi-4.1.4/bin/bt-mz.A.x'
+    num_tasks = 4
+    num_tasks_per_node = 2
+    num_cpus_per_task = 2
+    time_limit = '5m'
+    prerun_cmds = [
+        'ulimit -s unlimited'
+    ]
+    env_vars = {
+        'OMP_NUM_THREADS': '2',
+        'SLURM_MPI_TYPE': 'pmix_v2',
+        'SLURM_CPU_BIND': 'verbose'
+    }
+    reference = {
+        'discovery:main': {
+            'Mop/s_total': (26000, -0.1, None, 'Mop/s')
+        },
+        'discovery:epyc-64': {
+            'Mop/s_total': (45000, -0.1, None, 'Mop/s')
+        },
+        'discovery:gpu': {
+            'Mop/s_total': (26000, -0.1, None, 'Mop/s')
+        },
+        'endeavour:shared': {
+            'Mop/s_total': (26000, -0.1, None, 'Mop/s')
         }
-        self.sanity_patterns = sn.assert_found(r'SUCCESSFUL', self.stdout)
-        self.perf_patterns = {
-            'Mop/s total': sn.extractsingle(r'Mop/s total\s+=\s+(?P<mops_ret>\S+)', self.stdout, 'mops_ret', float)
-        }
-        self.reference = {
-            'discovery:main': {
-                'Mop/s total': (22000, -0.1, None, 'Mop/s')
-            },
-            'discovery:epyc-64': {
-                'Mop/s total': (46000, -0.1, None, 'Mop/s')
-            },
-            'endeavour:shared': {
-                'Mop/s total': (29000, -0.1, None, 'Mop/s')
-            }
-        }
+    }
+
+    @sanity_function
+    def assert_sanity(self):
+        return sn.assert_found(r'SUCCESSFUL', self.stdout)
+
+    @performance_function('Mop/s', perf_key = 'Mop/s_total')
+    def extract_perf(self):
+        return sn.extractsingle(r'Mop/s total\s+=\s+(?P<mops_ret>\S+)', self.stdout, 'mops_ret', float)
