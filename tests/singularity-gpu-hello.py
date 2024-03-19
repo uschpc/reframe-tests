d0ea35f89be27affe57360df3bd1efc6b5a2f5ba
Derek Ryan Strong <dstrong@usc.edu>
2024-03-19 16:42:18 -0700

    Update singularity-gpu-hello.py

--- tests/singularity-gpu-hello.py
+++ tests/singularity-gpu-hello.py
@@ -5,6 +5,7 @@ import reframe.utility.sanity as sn
 class Singularity_GPU_Hello(rfm.RunOnlyRegressionTest):
     descr = 'Singularity GPU access'
     valid_systems = [
+        'discovery:main',
         'discovery:gpu',
         'discovery:debug',
         'endeavour:cryoem',
@@ -14,7 +15,7 @@ class Singularity_GPU_Hello(rfm.RunOnlyRegressionTest):
         'PrgEnv-singularity'
     ]
     sourcesdir = None
-    executable = 'singularity exec --nv /spack/singularity/ood/centos7-xfce-oodv3.sif nvidia-smi'
+    executable = 'singularity exec --nv /spack/singularity/ood/centos7-xfce-oodv3.sif echo "Hello world from $(nvidia-smi -L)"'
     num_tasks = 1
     num_cpus_per_task = 1
     time_limit = '5m'
@@ -27,4 +28,4 @@ class Singularity_GPU_Hello(rfm.RunOnlyRegressionTest):
 
     @sanity_function
     def assert_sanity(self):
-        return sn.assert_found(r'NVIDIA-SMI', self.stdout)
+        return sn.assert_found(r'UUID', self.stdout)
