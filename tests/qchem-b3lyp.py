a709e29787fa739bb208e4ab348b57c1a8331ed9
Derek Ryan Strong <dstrong@usc.edu>
2025-03-03 17:15:40 -0800

    Update qchem-b3lyp.py

--- tests/qchem-b3lyp.py
+++ tests/qchem-b3lyp.py
@@ -24,13 +24,13 @@ class QChem_B3LYP(rfm.RunOnlyRegressionTest):
     valid_prog_environs = [
         "env-qchem"
     ]
-    sourcesdir = "./src/qchem-b3lyp"
+    sourcesdir = "src/qchem-b3lyp"
     executable = "qchem -nt $OMP_NUM_THREADS qchem-b3lyp.inp"
     num_tasks = 1
     num_cpus_per_task = 8
     time_limit = "5m"
     env_vars = {
-        "OMP_NUM_THREADS": "8"
+        "OMP_NUM_THREADS": "$SLURM_CPUS_PER_TASK"
     }
 
     @sanity_function
