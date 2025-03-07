b852d6a3603ecdf8fa7344560a49d370dc336d9f
Derek Ryan Strong <dstrong@usc.edu>
2025-03-07 12:16:20 -0800

    Update use-reframe.sh

--- scripts/use-reframe.sh
+++ scripts/use-reframe.sh
@@ -5,13 +5,17 @@ export PATH=/apps/reframe/reframe-4.7.4/bin:"$PATH"
 
 h="$(hostname -s)"
 if [[ "$h" == "discovery"* ]]; then
-    export RFM_CONFIG_FILES=/project/hpcroot/rfm/reframe-tests/config/discovery.py
+    dir=/project/hpcroot/rfm
+    export RFM_CONFIG_FILES="$dir"/reframe-tests/config/discovery.py
 elif [[ "$h" == "endeavour"* ]]; then
-    export RFM_CONFIG_FILES=/project/hpcroot/rfm/reframe-tests/config/endeavour.py
+    dir=/project/hpcroot/rfm
+    export RFM_CONFIG_FILES="$dir"/reframe-tests/config/endeavour.py
 elif [[ "$h" == "laguna"* ]]; then
-    export RFM_CONFIG_FILES=/project/jkhong_1307/rfm/reframe-tests/config/laguna.py
+    dir=/project/jkhong_1307/rfm
+    export RFM_CONFIG_FILES="$dir"/reframe-tests/config/laguna.py
 else
     echo "Error: Hostname not recognized"
     exit 1
 fi
-export RFM_CONFIG_FILES=/project/hpcroot/rfm/reframe-tests/config/shared/environments.py:"$RFM_CONFIG_FILES"
+export RFM_CONFIG_FILES="$dir"/reframe-tests/config/shared/environments.py:"$RFM_CONFIG_FILES"
+cd "$dir"
