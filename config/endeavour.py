63b8c2197f9acbf4b8ab78afb3e62e128103738c
Derek Ryan Strong <dstrong@usc.edu>
2025-03-18 17:18:30 -0700

    Add Endeavour allnodes partition

--- config/endeavour.py
+++ config/endeavour.py
@@ -23,6 +23,31 @@ site_configuration = {
             ],
             "partitions": [
                 {
+                    "name": "allnodes",
+                    "descr": "Endeavour allnodes partition",
+                    "scheduler": "slurm",
+                    "launcher": "srun",
+                    "access": [
+                        "--account=hpcroot",
+                        "--partition=allnodes"
+                    ],
+                    "environs": [
+                        "env-apptainer",
+                        "env-gcc-13.3.0",
+                        "env-gcc-13.3.0-openmpi-5.0.5",
+                        "env-gcc-13.3.0-mpich-4.2.2",
+                        "env-gcc-13.3.0-cuda-12.6.3",
+                        "env-julia",
+                        "env-python",
+                        "env-r",
+                        "env-matlab",
+                        "env-qchem",
+                        "env-curl",
+                        "env-fio",
+                        "env-ior"
+                    ]
+                },
+                {
                     "name": "shared",
                     "descr": "Endeavour shared partition",
                     "scheduler": "slurm",
