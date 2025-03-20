22819b7b270683419dda6f2888ddaf3cc00a4452
Derek Ryan Strong <dstrong@usc.edu>
2025-03-20 12:51:07 -0700

    Update install-reframe.sh

--- scripts/install-reframe.sh
+++ scripts/install-reframe.sh
@@ -1,18 +1,40 @@
 # Install ReFrame in current directory
-# Argument should be version number
 # e.g., bash install-reframe.sh 4.7.4
+
+set -eu
+
+if [[ "$#" -eq 0 ]]; then
+    echo "Error: no argument given"
+    echo "Argument should be ReFrame version"
+    exit 1
+fi
+
+ver="$1"
 
 module purge
 module load gcc/13.3.0 python/3.11.9 curl tar gzip
-ver="$1"
-curl -LO https://github.com/reframe-hpc/reframe/archive/refs/tags/v"$ver".tar.gz
+
+url="https://github.com/reframe-hpc/reframe/archive/refs/tags/v$ver.tar.gz"
+code="$(curl -sILo /dev/null -w "%{http_code}" "$url")"
+if [[ "$code" -ne 200 ]]; then
+    echo "Error: URL does not exist or not reachable"
+    echo "Check ReFrame version"
+    exit 1
+fi
+curl -sLO "$url"
+
+f="$(file -b v"$ver".tar.gz)"
+if [[ "$f" != *"gzip compressed data"* ]]; then
+    echo "Error: file is not gzipped archive"
+    exit 1
+fi
 tar -xf v"$ver".tar.gz
 rm v"$ver".tar.gz
-cd reframe-"$ver"
+
+cd reframe-"$ver" || { echo "Error: cd failure"; exit 1; }
 ./bootstrap.sh
 py="$(type -p python3)"
 sed -i "1s%.*%#\!${py}%" bin/reframe
+
 module purge
-cd ..
-chmod -R ug-w reframe-"$ver"
-reframe-"$ver"/bin/reframe -V
+echo "Installed ReFrame" "$(bin/reframe -V)"
