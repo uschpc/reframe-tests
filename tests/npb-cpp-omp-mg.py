a29a3c33640ce22ee9ef1d7244123dd515528c89
Derek Ryan Strong <dstrong@usc.edu>
2025-03-20 18:19:08 -0700

    Add NPB CPP OMP MG benchmark test

---
+++ tests/npb-cpp-omp-mg.py
@@ -0,0 +1,45 @@
+import reframe as rfm
+import reframe.utility.sanity as sn
+
+@rfm.simple_test
+class NPB_CPP_OMP_MG(rfm.RunOnlyRegressionTest):
+    descr = "NPB CPP OpenMP MG benchmark"
+    tags = {
+        "maintenance"
+    }
+    valid_systems = [
+        "discovery:allnodes",
+        "endeavour:allnodes"
+    ]
+    valid_prog_environs = [
+        "env-gcc-13.3.0"
+    ]
+    sourcesdir = "src/npb-cpp-omp-mg"
+    executable = "mg.D"
+    time_limit = "5m"
+    env_vars = {
+        "OMP_NUM_THREADS": "$SLURM_CPUS_ON_NODE"
+    }
+    prerun_cmds = [
+        "bash make-npb-cpp-omp-mg.sh"
+    ]
+    reference = {
+        "*": {
+            "Mop/s_total": (26000, -0.1, None, "Mop/s")
+        }
+    }
+
+    @run_before("run")
+    def set_job_options(self):
+        self.job.options += [
+            "--exclusive",
+            "--mem=0"
+        ]
+
+    @sanity_function
+    def assert_sanity(self):
+        return sn.assert_found(r"VERIFICATION SUCCESSFUL", self.stdout)
+
+    @performance_function("Mop/s", perf_key = "Mop/s_total")
+    def extract_perf(self):
+        return sn.extractsingle(r"Mop/s total\s+=\s+(?P<mops_ret>\S+)", self.stdout, "mops_ret", float)
---
+++ tests/src/npb-cpp-omp-mg/make-npb-cpp-omp-mg.sh
@@ -0,0 +1,5 @@
+cd "$TMPDIR"
+tar -xf /apps/reframe/resources/NPB/NPB-CPP-dad2c1d.tar.gz
+cd NPB-CPP/NPB-OMP
+make MG CLASS=D
+mv bin/mg.D "$SLURM_SUBMIT_DIR"
