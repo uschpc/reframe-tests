d5435593ca027f7f2aeecb3f59d581712508ad5a
Derek Ryan Strong <dstrong@usc.edu>
2025-03-21 16:28:07 -0700

    Update npb-mpi-ep-discovery-epyc.py

--- tests/npb-mpi-ep-discovery-epyc.py
+++ tests/npb-mpi-ep-discovery-epyc.py
@@ -27,7 +27,7 @@ class NPB_MPI_EP_Discovery_Epyc(rfm.RunOnlyRegressionTest):
 
     @sanity_function
     def assert_sanity(self):
-        return sn.assert_found(r"^SUCCESSFUL", self.stdout)
+        return sn.assert_found(r"Verification\s+=\s+SUCCESSFUL", self.stdout)
 
     @performance_function("Mop/s", perf_key = "Mop/s_total")
     def extract_perf(self):
