31f9202e8adbcbfabb229aa7854c16baeac4d58f
Derek Ryan Strong <dstrong@usc.edu>
2025-04-09 13:20:52 -0700

    Add NPB OMP EP test

---
+++ tests/npb-omp-ep.py
@@ -0,0 +1,57 @@
+# NAS Parallel Benchmarks OpenMP EP (embarrassingly parallel) benchmark test
+# Purpose of test
+# - Test compiler module access
+# - Test building Fortran/OpenMP program
+# - Test running Fortran/OpenMP program
+# - Test node performance targeting maximum flop/s
+# Notes
+# - Requires a power-of-two number of CPU cores
+
+import reframe as rfm
+import reframe.utility.sanity as sn
+
+@rfm.simple_test
+class NPB_OMP_EP(rfm.RunOnlyRegressionTest):
+    descr = "NPB OpenMP EP benchmark"
+    tags = {
+        "maintenance",
+        "performance",
+        "singlenode"
+    }
+    valid_systems = [
+        "discovery:allnodes",
+        "endeavour:allnodes",
+        "pathfinder:allnodes"
+    ]
+    valid_prog_environs = [
+        "env-gcc-13.3.0"
+    ]
+    sourcesdir = "src/npb-omp-ep"
+    executable = "ep.D.x"
+    time_limit = "5m"
+    env_vars = {
+        "OMP_NUM_THREADS": "$SLURM_CPUS_ON_NODE"
+    }
+    prerun_cmds = [
+        "bash make-npb-omp-ep.sh"
+    ]
+    reference = {
+        "*": {
+            "Mop/s_total": (900, -0.1, None, "Mop/s")
+        }
+    }
+
+    @run_before("run")
+    def set_job_options(self):
+        self.job.options += [
+            "--exclusive",
+            "--mem=0"
+        ]
+
+    @sanity_function
+    def assert_sanity(self):
+        return sn.assert_found(r"Verification\s+=\s+SUCCESSFUL", self.stdout)
+
+    @performance_function("Mop/s", perf_key = "Mop/s_total")
+    def extract_perf(self):
+        return sn.extractsingle(r"Mop/s total\s+=\s+(?P<mops_ret>\S+)", self.stdout, "mops_ret", float)
