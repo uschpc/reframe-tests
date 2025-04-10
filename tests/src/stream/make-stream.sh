a06cf9b1e1088ad353330faea6abbb5f0aaf4136
Derek Ryan Strong <dstrong@usc.edu>
2025-04-10 15:41:53 -0700

    Add stream-epyc-7542.py

--- tests/src/stream/make-stream.sh
+++ tests/src/stream/make-stream.sh
@@ -5,11 +5,9 @@ set -e
 cd "$TMPDIR"
 tar -xf /apps/reframe/resources/STREAM/STREAM-6703f75.tar.gz
 cd STREAM
-if scontrol show node "$(hostname -s)" | grep "epyc-7513"; then
+if scontrol show node "$(hostname -s)" | grep -E "epyc-7542|epyc-7513"; then
     sed -i '/CFLAGS =/c\CFLAGS = -O2 -fopenmp -march=native -mcmodel=large -DSTREAM_ARRAY_SIZE=1048576000 -DSTREAM_TYPE=double -DNTIMES=100' Makefile
-elif scontrol show node "$(hostname -s)" | grep "epyc-9554"; then
-    sed -i '/CFLAGS =/c\CFLAGS = -O2 -fopenmp -march=native -mcmodel=large -DSTREAM_ARRAY_SIZE=2097152000 -DSTREAM_TYPE=double -DNTIMES=100' Makefile
-elif scontrol show node "$(hostname -s)" | grep "epyc-9354"; then
+elif scontrol show node "$(hostname -s)" | grep -E "epyc-9354|epyc-9554"; then
     sed -i '/CFLAGS =/c\CFLAGS = -O2 -fopenmp -march=native -mcmodel=large -DSTREAM_ARRAY_SIZE=2097152000 -DSTREAM_TYPE=double -DNTIMES=100' Makefile
 else
     echo "Error: CPU model not yet supported for ReFrame STREAM test"
