81203a8fce68989b7c709add5374219223388ab9
Derek Ryan Strong <dstrong@usc.edu>
2025-08-21 17:12:56 -0700

    Add git-clone-scratch.py

--- tests/src/git-clone/git-clone.sh
+++ tests/src/git-clone/git-clone.sh
@@ -8,6 +8,8 @@ elif [[ "$1" == "home1" ]]; then
     dir="/home1/$USER"
 elif [[ "$1" == "scratch1" ]]; then
     dir="/scratch1/$USER"
+elif [[ "$1" == "scratch" ]]; then
+    dir="/scratch/$USER"
 elif [[ "$1" == "project" ]]; then
     if [[ "$SLURM_SUBMIT_HOST" == "discovery"* ]] || [[ "$SLURM_SUBMIT_HOST" == "endeavour"* ]]; then
         dir="/project/wjendrze_120/reframe/tmp"
