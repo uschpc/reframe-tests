abff6b00a170a149b9fe1c13b5a39ef5851ce4c1
Derek Ryan Strong <dstrong@usc.edu>
2025-09-12 16:13:54 -0700

    Add Endeavour partitions for node types

--- config/endeavour.py
+++ config/endeavour.py
@@ -40,6 +40,7 @@ site_configuration = {
                     ],
                     "max_jobs": 5000,
                     "environs": [
+                        "env-none",
                         "env-apptainer",
                         "env-gcc-13.3.0",
                         "env-gcc-13.3.0-openmpi-5.0.5",
@@ -55,7 +56,496 @@ site_configuration = {
                         "env-ior",
                         "env-omb"
                     ]
-                }
+                },
+                {
+                    "name": "epyc-9554",
+                    "descr": "Endeavour epyc-9554 nodes",
+                    "scheduler": "slurm",
+                    "launcher": "srun",
+                    "access": [
+                        "--account=hpcroot",
+                        "--partition=allnodes",
+                        "--constraint=epyc-9554"
+                    ],
+                    "max_jobs": 5000,
+                    "environs": [
+                        "env-none",
+                        "env-apptainer",
+                        "env-gcc-13.3.0",
+                        "env-gcc-13.3.0-openmpi-5.0.5",
+                        "env-gcc-13.3.0-mpich-4.2.2",
+                        "env-gcc-13.3.0-cuda-12.6.3",
+                        "env-julia",
+                        "env-python",
+                        "env-r",
+                        "env-matlab",
+                        "env-qchem",
+                        "env-git",
+                        "env-fio",
+                        "env-ior",
+                        "env-omb"
+                    ]
+                },
+                {
+                    "name": "epyc-9354",
+                    "descr": "Endeavour epyc-9354 nodes",
+                    "scheduler": "slurm",
+                    "launcher": "srun",
+                    "access": [
+                        "--account=hpcroot",
+                        "--partition=allnodes",
+                        "--constraint=epyc-9354"
+                    ],
+                    "max_jobs": 5000,
+                    "environs": [
+                        "env-none",
+                        "env-apptainer",
+                        "env-gcc-13.3.0",
+                        "env-gcc-13.3.0-openmpi-5.0.5",
+                        "env-gcc-13.3.0-mpich-4.2.2",
+                        "env-gcc-13.3.0-cuda-12.6.3",
+                        "env-julia",
+                        "env-python",
+                        "env-r",
+                        "env-matlab",
+                        "env-qchem",
+                        "env-git",
+                        "env-fio",
+                        "env-ior",
+                        "env-omb"
+                    ]
+                },
+                {
+                    "name": "epyc-9124",
+                    "descr": "Endeavour epyc-9124 nodes",
+                    "scheduler": "slurm",
+                    "launcher": "srun",
+                    "access": [
+                        "--account=hpcroot",
+                        "--partition=allnodes",
+                        "--constraint=epyc-9124"
+                    ],
+                    "max_jobs": 5000,
+                    "environs": [
+                        "env-none",
+                        "env-apptainer",
+                        "env-gcc-13.3.0",
+                        "env-gcc-13.3.0-openmpi-5.0.5",
+                        "env-gcc-13.3.0-mpich-4.2.2",
+                        "env-gcc-13.3.0-cuda-12.6.3",
+                        "env-julia",
+                        "env-python",
+                        "env-r",
+                        "env-matlab",
+                        "env-qchem",
+                        "env-git",
+                        "env-fio",
+                        "env-ior",
+                        "env-omb"
+                    ]
+                },
+                {
+                    "name": "epyc-7643",
+                    "descr": "Endeavour epyc-7643 nodes",
+                    "scheduler": "slurm",
+                    "launcher": "srun",
+                    "access": [
+                        "--account=hpcroot",
+                        "--partition=allnodes",
+                        "--constraint=epyc-7643"
+                    ],
+                    "max_jobs": 5000,
+                    "environs": [
+                        "env-none",
+                        "env-apptainer",
+                        "env-gcc-13.3.0",
+                        "env-gcc-13.3.0-openmpi-5.0.5",
+                        "env-gcc-13.3.0-mpich-4.2.2",
+                        "env-gcc-13.3.0-cuda-12.6.3",
+                        "env-julia",
+                        "env-python",
+                        "env-r",
+                        "env-matlab",
+                        "env-qchem",
+                        "env-git",
+                        "env-fio",
+                        "env-ior",
+                        "env-omb"
+                    ]
+                },
+                {
+                    "name": "epyc-7513",
+                    "descr": "Endeavour epyc-7513 nodes",
+                    "scheduler": "slurm",
+                    "launcher": "srun",
+                    "access": [
+                        "--account=hpcroot",
+                        "--partition=allnodes",
+                        "--constraint=epyc-7513"
+                    ],
+                    "max_jobs": 5000,
+                    "environs": [
+                        "env-none",
+                        "env-apptainer",
+                        "env-gcc-13.3.0",
+                        "env-gcc-13.3.0-openmpi-5.0.5",
+                        "env-gcc-13.3.0-mpich-4.2.2",
+                        "env-julia",
+                        "env-python",
+                        "env-r",
+                        "env-matlab",
+                        "env-qchem",
+                        "env-git",
+                        "env-fio",
+                        "env-ior",
+                        "env-omb"
+                    ]
+                },
+                {
+                    "name": "epyc-7313",
+                    "descr": "Endeavour epyc-7313 nodes",
+                    "scheduler": "slurm",
+                    "launcher": "srun",
+                    "access": [
+                        "--account=hpcroot",
+                        "--partition=allnodes",
+                        "--constraint=epyc-7313"
+                    ],
+                    "max_jobs": 5000,
+                    "environs": [
+                        "env-none",
+                        "env-apptainer",
+                        "env-gcc-13.3.0",
+                        "env-gcc-13.3.0-openmpi-5.0.5",
+                        "env-gcc-13.3.0-mpich-4.2.2",
+                        "env-gcc-13.3.0-cuda-12.6.3",
+                        "env-julia",
+                        "env-python",
+                        "env-r",
+                        "env-matlab",
+                        "env-qchem",
+                        "env-git",
+                        "env-fio",
+                        "env-ior",
+                        "env-omb"
+                    ]
+                },
+                {
+                    "name": "epyc-7502p",
+                    "descr": "Endeavour epyc-7502p nodes",
+                    "scheduler": "slurm",
+                    "launcher": "srun",
+                    "access": [
+                        "--account=hpcroot",
+                        "--partition=allnodes",
+                        "--constraint=epyc-7502p"
+                    ],
+                    "max_jobs": 5000,
+                    "environs": [
+                        "env-none",
+                        "env-apptainer",
+                        "env-gcc-13.3.0",
+                        "env-gcc-13.3.0-openmpi-5.0.5",
+                        "env-gcc-13.3.0-mpich-4.2.2",
+                        "env-gcc-13.3.0-cuda-12.6.3",
+                        "env-julia",
+                        "env-python",
+                        "env-r",
+                        "env-matlab",
+                        "env-qchem",
+                        "env-git",
+                        "env-fio",
+                        "env-ior",
+                        "env-omb"
+                    ]
+                },
+                {
+                    "name": "epyc-7502",
+                    "descr": "Endeavour epyc-7502 nodes",
+                    "scheduler": "slurm",
+                    "launcher": "srun",
+                    "access": [
+                        "--account=hpcroot",
+                        "--partition=allnodes",
+                        "--constraint=epyc-7502"
+                    ],
+                    "max_jobs": 5000,
+                    "environs": [
+                        "env-none",
+                        "env-apptainer",
+                        "env-gcc-13.3.0",
+                        "env-gcc-13.3.0-openmpi-5.0.5",
+                        "env-gcc-13.3.0-mpich-4.2.2",
+                        "env-gcc-13.3.0-cuda-12.6.3",
+                        "env-julia",
+                        "env-python",
+                        "env-r",
+                        "env-matlab",
+                        "env-qchem",
+                        "env-git",
+                        "env-fio",
+                        "env-ior",
+                        "env-omb"
+                    ]
+                },
+                {
+                    "name": "xeon-8358",
+                    "descr": "Endeavour xeon-8358 nodes",
+                    "scheduler": "slurm",
+                    "launcher": "srun",
+                    "access": [
+                        "--account=hpcroot",
+                        "--partition=allnodes",
+                        "--constraint=xeon-8358"
+                    ],
+                    "max_jobs": 5000,
+                    "environs": [
+                        "env-none",
+                        "env-apptainer",
+                        "env-gcc-13.3.0",
+                        "env-gcc-13.3.0-openmpi-5.0.5",
+                        "env-gcc-13.3.0-mpich-4.2.2",
+                        "env-gcc-13.3.0-cuda-12.6.3",
+                        "env-julia",
+                        "env-python",
+                        "env-r",
+                        "env-matlab",
+                        "env-qchem",
+                        "env-git",
+                        "env-fio",
+                        "env-ior",
+                        "env-omb"
+                    ]
+                },
+                {
+                    "name": "xeon-6348",
+                    "descr": "Endeavour xeon-6348 nodes",
+                    "scheduler": "slurm",
+                    "launcher": "srun",
+                    "access": [
+                        "--account=hpcroot",
+                        "--partition=allnodes",
+                        "--constraint=xeon-6348"
+                    ],
+                    "max_jobs": 5000,
+                    "environs": [
+                        "env-none",
+                        "env-apptainer",
+                        "env-gcc-13.3.0",
+                        "env-gcc-13.3.0-openmpi-5.0.5",
+                        "env-gcc-13.3.0-mpich-4.2.2",
+                        "env-gcc-13.3.0-cuda-12.6.3",
+                        "env-julia",
+                        "env-python",
+                        "env-r",
+                        "env-matlab",
+                        "env-qchem",
+                        "env-git",
+                        "env-fio",
+                        "env-ior",
+                        "env-omb"
+                    ]
+                },
+                {
+                    "name": "xeon-6338",
+                    "descr": "Endeavour xeon-6338 nodes",
+                    "scheduler": "slurm",
+                    "launcher": "srun",
+                    "access": [
+                        "--account=hpcroot",
+                        "--partition=allnodes",
+                        "--constraint=xeon-6338"
+                    ],
+                    "max_jobs": 5000,
+                    "environs": [
+                        "env-none",
+                        "env-apptainer",
+                        "env-gcc-13.3.0",
+                        "env-gcc-13.3.0-openmpi-5.0.5",
+                        "env-gcc-13.3.0-mpich-4.2.2",
+                        "env-gcc-13.3.0-cuda-12.6.3",
+                        "env-julia",
+                        "env-python",
+                        "env-r",
+                        "env-matlab",
+                        "env-qchem",
+                        "env-git",
+                        "env-fio",
+                        "env-ior",
+                        "env-omb"
+                    ]
+                },
+                {
+                    "name": "xeon-6226r",
+                    "descr": "Endeavour xeon-6226r nodes",
+                    "scheduler": "slurm",
+                    "launcher": "srun",
+                    "access": [
+                        "--account=hpcroot",
+                        "--partition=allnodes",
+                        "--constraint=xeon-6226r"
+                    ],
+                    "max_jobs": 5000,
+                    "environs": [
+                        "env-none",
+                        "env-apptainer",
+                        "env-gcc-13.3.0",
+                        "env-gcc-13.3.0-openmpi-5.0.5",
+                        "env-gcc-13.3.0-mpich-4.2.2",
+                        "env-gcc-13.3.0-cuda-12.6.3",
+                        "env-julia",
+                        "env-python",
+                        "env-r",
+                        "env-matlab",
+                        "env-qchem",
+                        "env-git",
+                        "env-fio",
+                        "env-ior",
+                        "env-omb"
+                    ]
+                },
+                {
+                    "name": "xeon-6148",
+                    "descr": "Endeavour xeon-6148 nodes",
+                    "scheduler": "slurm",
+                    "launcher": "srun",
+                    "access": [
+                        "--account=hpcroot",
+                        "--partition=allnodes",
+                        "--constraint=xeon-6148"
+                    ],
+                    "max_jobs": 5000,
+                    "environs": [
+                        "env-none",
+                        "env-apptainer",
+                        "env-gcc-13.3.0",
+                        "env-gcc-13.3.0-openmpi-5.0.5",
+                        "env-gcc-13.3.0-mpich-4.2.2",
+                        "env-julia",
+                        "env-python",
+                        "env-r",
+                        "env-matlab",
+                        "env-qchem",
+                        "env-git",
+                        "env-fio",
+                        "env-ior",
+                        "env-omb"
+                    ]
+                },
+                {
+                    "name": "xeon-6130",
+                    "descr": "Endeavour xeon-6130 nodes",
+                    "scheduler": "slurm",
+                    "launcher": "srun",
+                    "access": [
+                        "--account=hpcroot",
+                        "--partition=allnodes",
+                        "--constraint=xeon-6130"
+                    ],
+                    "max_jobs": 5000,
+                    "environs": [
+                        "env-none",
+                        "env-apptainer",
+                        "env-gcc-13.3.0",
+                        "env-gcc-13.3.0-openmpi-5.0.5",
+                        "env-gcc-13.3.0-mpich-4.2.2",
+                        "env-gcc-13.3.0-cuda-12.6.3",
+                        "env-julia",
+                        "env-python",
+                        "env-r",
+                        "env-matlab",
+                        "env-qchem",
+                        "env-git",
+                        "env-fio",
+                        "env-ior",
+                        "env-omb"
+                    ]
+                },
+                {
+                    "name": "xeon-5118",
+                    "descr": "Endeavour xeon-5118 nodes",
+                    "scheduler": "slurm",
+                    "launcher": "srun",
+                    "access": [
+                        "--account=hpcroot",
+                        "--partition=allnodes",
+                        "--constraint=xeon-5118"
+                    ],
+                    "max_jobs": 5000,
+                    "environs": [
+                        "env-none",
+                        "env-apptainer",
+                        "env-gcc-13.3.0",
+                        "env-gcc-13.3.0-openmpi-5.0.5",
+                        "env-gcc-13.3.0-mpich-4.2.2",
+                        "env-julia",
+                        "env-python",
+                        "env-r",
+                        "env-matlab",
+                        "env-qchem",
+                        "env-git",
+                        "env-fio",
+                        "env-ior",
+                        "env-omb"
+                    ]
+                },
+                {
+                    "name": "xeon-4116",
+                    "descr": "Endeavour xeon-4116 nodes",
+                    "scheduler": "slurm",
+                    "launcher": "srun",
+                    "access": [
+                        "--account=hpcroot",
+                        "--partition=allnodes",
+                        "--constraint=xeon-4116"
+                    ],
+                    "max_jobs": 5000,
+                    "environs": [
+                        "env-none",
+                        "env-apptainer",
+                        "env-gcc-13.3.0",
+                        "env-gcc-13.3.0-openmpi-5.0.5",
+                        "env-gcc-13.3.0-mpich-4.2.2",
+                        "env-julia",
+                        "env-python",
+                        "env-r",
+                        "env-matlab",
+                        "env-qchem",
+                        "env-git",
+                        "env-fio",
+                        "env-ior",
+                        "env-omb"
+                    ]
+                },
+                {
+                    "name": "xeon-2640v4",
+                    "descr": "Endeavour xeon-2640v4 nodes",
+                    "scheduler": "slurm",
+                    "launcher": "srun",
+                    "access": [
+                        "--account=hpcroot",
+                        "--partition=allnodes",
+                        "--constraint=xeon-2640v4"
+                    ],
+                    "max_jobs": 5000,
+                    "environs": [
+                        "env-none",
+                        "env-apptainer",
+                        "env-gcc-13.3.0",
+                        "env-gcc-13.3.0-openmpi-5.0.5",
+                        "env-gcc-13.3.0-mpich-4.2.2",
+                        "env-gcc-13.3.0-cuda-12.6.3",
+                        "env-julia",
+                        "env-python",
+                        "env-r",
+                        "env-matlab",
+                        "env-qchem",
+                        "env-git",
+                        "env-fio",
+                        "env-ior",
+                        "env-omb"
+                    ]
+                },
             ]
         }
     ],
