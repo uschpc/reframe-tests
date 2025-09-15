165e4c4b17dbd343a5408361d8db81fb1b8bc925
Derek Ryan Strong <dstrong@usc.edu>
2025-09-15 14:47:46 -0700

    Revise Endeavour perf target for fio-randrw-project2.py

--- tests/fio-randrw-project2.py
+++ tests/fio-randrw-project2.py
@@ -21,7 +21,7 @@ class fio_randrw_project2(rfm.RunOnlyRegressionTest):
     }
     valid_systems = [
         "discovery:epyc-7513",
-        "endeavour:allnodes"
+        "endeavour:epyc-9554"
     ]
     valid_prog_environs = [
         "env-fio"
@@ -36,9 +36,9 @@ class fio_randrw_project2(rfm.RunOnlyRegressionTest):
             "avg_write_speed": (320.0, -0.1, None, "MiB/sec"),
             "avg_read_speed": (320.0, -0.1, None, "MiB/sec")
         },
-        "*": {
-            "avg_write_speed": (320.00, -0.1, None, "MiB/sec"),
-            "avg_read_speed": (320.00, -0.1, None, "MiB/sec")
+        "endeavour:epyc-9554": {
+            "avg_write_speed": (293.0, -0.1, None, "MiB/sec"),
+            "avg_read_speed": (293.0, -0.1, None, "MiB/sec")
         }
     }
 
