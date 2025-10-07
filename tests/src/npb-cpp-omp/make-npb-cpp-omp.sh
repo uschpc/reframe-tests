8832c6463f43133af3c31b3c428fe90c9b3f850c
Derek Ryan Strong <dstrong@usc.edu>
2025-10-07 14:28:32 -0700

    Add -march=native to NPB builds

--- tests/src/npb-cpp-omp/make-npb-cpp-omp.sh
+++ tests/src/npb-cpp-omp/make-npb-cpp-omp.sh
@@ -5,5 +5,6 @@ set -e
 cd "$TMPDIR"
 tar -xf /apps/reframe/resources/NPB/NPB-CPP-dad2c1d.tar.gz
 cd NPB-CPP/NPB-OMP
+sed -i 's/-O3/-O3 -march=native/' config/make.def
 make "$1" CLASS="$2"
 mv bin/"$1"."$2" "$SLURM_SUBMIT_DIR"
--- tests/src/npb-mpi/make-npb-mpi.sh
+++ tests/src/npb-mpi/make-npb-mpi.sh
@@ -6,5 +6,6 @@ cd "$TMPDIR"
 tar -xf /apps/reframe/resources/NPB/NPB3.4.3.tar.gz
 cd NPB3.4.3/NPB3.4-MPI
 mv config/make.def.template config/make.def
+sed -i 's/-O3/-O3 -march=native/' config/make.def
 make "$1" CLASS="$2"
 mv bin/"$1"."$2".x "$SLURM_SUBMIT_DIR"
