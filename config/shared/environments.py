5a1b211080741846d8c05b30f863b9e72dbe336b
Derek Ryan Strong <dstrong@usc.edu>
2025-12-09 17:30:10 -0800

    Update to apptainer/1.4.5

--- config/shared/environments.py
+++ config/shared/environments.py
@@ -10,7 +10,7 @@ site_configuration = {
         {
              "name": "env-apptainer",
              "modules": [
-                 "apptainer/1.4.4"
+                 "apptainer/1.4.5"
              ]
         },
         {
