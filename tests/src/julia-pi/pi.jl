05cf4bb83673600b423833151a1e83259ae9de19
Derek Ryan Strong <dstrong@usc.edu>
2025-12-18 15:47:51 -0800

    Update pi.jl

--- tests/src/julia-pi/pi.jl
+++ tests/src/julia-pi/pi.jl
@@ -12,16 +12,18 @@ end
 
 # Estimate pi using n points
 function est_pi(n)
-    nt = nthreads()
-    ps = zeros(nt)
-    # Each thread gets (n / nt) number of points
-    @threads for i in 1:nt
-        ps[threadid()] += monte_carlo_pi_part(ceil(Int, n / nt))
+    # One task per thread
+    # One chunk of points per task
+    nchunks = nthreads()
+    chunk = ceil(Int, n / nchunks)
+    tasks = map(1:nchunks) do task
+        @spawn monte_carlo_pi_part(chunk)
     end
-    est = 4 * (sum(ps) / n)
-    println("Estimate of pi: ", est)
+    total = mapreduce(fetch, +, tasks)
+    return 4 * (total / n)
 end
 
-elapsed = @elapsed est_pi(5000000000)
+res = @timed est_pi(5000000000)
 
-println("Elapsed time: ", elapsed)
+println("Estimate of pi: ", res.value)
+println("Elapsed time: ", res.time)
