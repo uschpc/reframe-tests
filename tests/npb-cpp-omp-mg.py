724f23cd1f59493c786b2d895974c9d36bbf71a6
Derek Ryan Strong <dstrong@usc.edu>
2026-02-11 13:11:58 -0800

    Update npb-cpp-omp-mg.py

--- tests/npb-cpp-omp-mg.py
+++ tests/npb-cpp-omp-mg.py
@@ -7,6 +7,7 @@
 # - Test node performance for differential equations solver
 #   using multigrid method
 # Notes
+# - https://github.com/GMAP/NPB-CPP
 # - Requires a power-of-two number of CPU cores
 
 import reframe as rfm
@@ -21,44 +22,17 @@ class npb_cpp_omp_mg(rfm.RunOnlyRegressionTest):
         "singlenode"
     }
     valid_systems = [
-        "discovery:epyc-9534",
-        "discovery:epyc-9354",
         "discovery:epyc-7513",
-        "discovery:epyc-7313",
-        "discovery:epyc-7542",
-        "discovery:epyc-7282",
-        "discovery:xeon-6130",
-        "discovery:xeon-4116",
-        "discovery:xeon-2640v4",
-        "endeavour:epyc-9554-128c",
-        "endeavour:epyc-9554-64c",
-        "endeavour:epyc-9355",
-        "endeavour:epyc-9354",
-        "endeavour:epyc-9124",
-        "endeavour:epyc-7643",
         "endeavour:epyc-7513",
-        "endeavour:epyc-7313",
-        "endeavour:epyc-7502p",
-        "endeavour:epyc-7502",
-        "endeavour:xeon-8358",
-        "endeavour:xeon-6348",
-        "endeavour:xeon-6338",
-        "endeavour:xeon-6226r",
-        "endeavour:xeon-6148",
-        "endeavour:xeon-6130",
-        "endeavour:xeon-5118",
-        "endeavour:xeon-4116",
-        "endeavour:xeon-2640v4",
         "pathfinder:xeon-2640v3",
-        "laguna:epyc-9554",
-        "laguna:epyc-9354"
+        "laguna:epyc-9554"
     ]
     valid_prog_environs = [
         "env-gcc-14.3.0",
         "env-gcc-13.3.0"
     ]
     sourcesdir = "src/npb-cpp-omp"
-    executable = "mg.D"
+    executable = "$TMPDIR/NPB-CPP/NPB-OMP/bin/mg.D"
     time_limit = "5m"
     env_vars = {
         "OMP_NUM_THREADS": "$SLURM_CPUS_ON_NODE"
@@ -67,98 +41,17 @@ class npb_cpp_omp_mg(rfm.RunOnlyRegressionTest):
         "bash make-npb-cpp-omp.sh mg D"
     ]
     reference = {
-        "discovery:epyc-9534": {
-            "Mop/s_total": (80000, -0.1, 0.1, "Mop/s")
-        },
-        "discovery:epyc-9354": {
-            "Mop/s_total": (80000, -0.1, 0.1, "Mop/s")
-        },
         "discovery:epyc-7513": {
-            "Mop/s_total": (80000, -0.1, 0.1, "Mop/s")
-        },
-        "discovery:epyc-7313": {
-            "Mop/s_total": (27000, -0.1, 0.1, "Mop/s")
-        },
-        "discovery:epyc-7542": {
-            "Mop/s_total": (70000, -0.1, 0.1, "Mop/s")
-        },
-        "discovery:epyc-7282": {
-            "Mop/s_total": (27000, -0.1, 0.1, "Mop/s")
-        },
-        "discovery:xeon-6130": {
-            "Mop/s_total": (26000, -0.1, 0.1, "Mop/s")
-        },
-        "discovery:xeon-4116": {
-            "Mop/s_total": (28239, -0.1, 0.1, "Mop/s")
-        },
-        "discovery:xeon-2640v4": {
-            "Mop/s_total": (23000, -0.1, 0.1, "Mop/s")
-        },
-        "endeavour:epyc-9554-128c": {
-            "Mop/s_total": (174628, -0.1, 0.1, "Mop/s")
-        },
-        "endeavour:epyc-9554-64c": {
-            "Mop/s_total": (107332, -0.1, 0.1, "Mop/s")
-        },
-        "endeavour:epyc-9355": {
-            "Mop/s_total": (257574, -0.1, 0.1, "Mop/s")
-        },
-        "endeavour:epyc-9354": {
-            "Mop/s_total": (85118, -0.1, 0.1, "Mop/s")
-        },
-        "endeavour:epyc-9124": {
-            "Mop/s_total": (84972, -0.1, 0.1, "Mop/s")
-        },
-        "endeavour:epyc-7643": {
-            "Mop/s_total": (60602, -0.1, 0.1, "Mop/s")
+            "Mop/s_total": (63000, -0.1, 0.1, "Mop/s")
         },
         "endeavour:epyc-7513": {
-            "Mop/s_total": (60602, -0.1, 0.1, "Mop/s")
-        },
-        "endeavour:epyc-7313": {
-            "Mop/s_total": (23000, -0.1, 0.1, "Mop/s")
-        },
-        "endeavour:epyc-7502p": {
-            "Mop/s_total": (36960, -0.1, 0.1, "Mop/s")
-        },
-        "endeavour:epyc-7502": {
-            "Mop/s_total": (36960, -0.1, 0.1, "Mop/s")
-        },
-        "endeavour:xeon-8358": {
-            "Mop/s_total": (55000, -0.1, 0.1, "Mop/s")
-        },
-        "endeavour:xeon-6348": {
-            "Mop/s_total": (50721, -0.1, 0.1, "Mop/s")
-        },
-        "endeavour:xeon-6338": {
-            "Mop/s_total": (50721, -0.1, 0.1, "Mop/s")
-        },
-        "endeavour:xeon-6226r": {
-            "Mop/s_total": (50721, -0.1, 0.1, "Mop/s")
-        },
-        "endeavour:xeon-6148": {
-            "Mop/s_total": (45128, -0.1, 0.1, "Mop/s")
-        },
-        "endeavour:xeon-6130": {
-            "Mop/s_total": (45592, -0.1, 0.1, "Mop/s")
-        },
-        "endeavour:xeon-5118": {
-            "Mop/s_total": (27578, -0.1, 0.1, "Mop/s")
-        },
-        "endeavour:xeon-4116": {
-            "Mop/s_total": (35609, -0.1, 0.1, "Mop/s")
-        },
-        "endeavour:xeon-2640v4": {
-            "Mop/s_total": (26018, -0.1, 0.1, "Mop/s")
+            "Mop/s_total": (63000, -0.1, 0.1, "Mop/s")
         },
         "pathfinder:xeon-2640v3": {
-            "Mop/s_total": (21660, -0.1, 0.1, "Mop/s")
+            "Mop/s_total": (22000, -0.1, 0.1, "Mop/s")
         },
         "laguna:epyc-9554": {
-            "Mop/s_total": (166563, -0.1, 0.1, "Mop/s")
-        },
-        "laguna:epyc-9354": {
-            "Mop/s_total": (185523, -0.1, 0.1, "Mop/s")
+            "Mop/s_total": (166000, -0.1, 0.1, "Mop/s")
         }
     }
 
