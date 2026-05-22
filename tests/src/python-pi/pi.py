1f20cd42a6040bb156a8e01c43b548d8a6becd9b
Derek Ryan Strong <dstrong@usc.edu>
2026-05-21 17:16:32 -0700

    Update pi.py

--- tests/src/python-pi/pi.py
+++ tests/src/python-pi/pi.py
@@ -1,49 +1,40 @@
-# Estimate pi in parallel using multiple cores
+# Estimate pi using Monte Carlo method in parallel
 
-from multiprocessing import set_start_method, Pool
+import concurrent.futures
 import os
 import random
 import time
 
-# Calculate the number of points in the unit circle out of n points
-def monte_carlo_pi_part(n):
+# Calculate number of points in the unit circle out of n points
+def monte_carlo_points(n):
+    rand = random.random
     count = 0
-
-    for i in range(n):
-        x = random.random()
-        y = random.random()
-
-        # If within the unit circle
-        if (x * x) + (y * y) <= 1:
-            count = count + 1
-
+    for _ in range(n):
+        x = rand()
+        y = rand()
+        if (x * x) + (y * y) <= 1.0:
+            count += 1
     return count
 
-if __name__=="__main__":
-    set_start_method("fork")
+# Estimate pi using n points
+def est_pi(n):
+    # Chunk number of points for parallel processing
+    # Set number of chunks to number of cores
+    nchunks = int(os.environ["SLURM_CPUS_PER_TASK"])
+    quo, rem = divmod(n, nchunks)
+    chunks = [quo] * nchunks
+    chunks[0] += rem
+    with concurrent.futures.ProcessPoolExecutor(nchunks) as executor:
+        total = sum(executor.map(monte_carlo_points, chunks))
+    return 4.0 * total / n
 
-    cores = int(os.environ["SLURM_CPUS_PER_TASK"])
+if __name__ == "__main__":
+    start = time.perf_counter()
 
-    start = time.time()
+    res = est_pi(5000000000)
 
-    # Number of points to use for the pi estimation
-    n = 5000000000
-
-    # Create iterable list
-    # Each worker process gets (n / cores) number of points
-    part_count = [n // cores] * cores
-
-    # Create the worker pool
-    pool = Pool(processes = cores)
-
-    # Parallel map
-    count = pool.map(monte_carlo_pi_part, part_count)
-
-    est = 4 * (sum(count) / (n * 1.0))
-
-    end = time.time()
-
+    end = time.perf_counter()
     elapsed = end - start
 
-    print("Estimate of pi:", est)
+    print("Estimate of pi:", res)
     print("Elapsed time:", elapsed)
