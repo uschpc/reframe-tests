ffe63c56768443ae80f0aa2531488a3d54af76cf
Derek Ryan Strong <dstrong@usc.edu>
2026-05-21 17:19:13 -0700

    Update pi.jl

--- tests/src/julia-pi/pi.jl
+++ tests/src/julia-pi/pi.jl
@@ -1,26 +1,32 @@
-# Estimate pi in parallel using multiple threads
+# Estimate pi using Monte Carlo method in parallel
 
 using Base.Threads
 using Random
 
-# Calculate the number of points in the unit circle out of n points
-function monte_carlo_pi_part(n)
-    rng = Xoshiro()
-    # If within the unit circle
-    return sum(rand(rng)^2 + rand(rng)^2 < 1 for i = 1:n)
+# Calculate number of points in the unit circle out of n points
+function monte_carlo_points(n)
+    count = 0
+    for _ in 1:n
+        x = rand()
+        y = rand()
+        if (x * x) + (y * y) <= 1.0
+            count += 1
+        end
+    end
+    return count
 end
 
 # Estimate pi using n points
 function est_pi(n)
-    # One task per thread
-    # One chunk of points per task
+    # Chunk number of points for parallel processing
+    # Set number of chunks to number of threads
     nchunks = nthreads()
-    chunk = ceil(Int, n / nchunks)
-    tasks = map(1:nchunks) do task
-        @spawn monte_carlo_pi_part(chunk)
-    end
-    total = mapreduce(fetch, +, tasks)
-    return 4 * (total / n)
+    quo, rem = divrem(n, nchunks)
+    chunks = fill(quo, nchunks)
+    chunks[1] += rem
+    tasks = [@spawn monte_carlo_points(chunk) for chunk in chunks]
+    total = sum(fetch, tasks)
+    return 4.0 * total / n
 end
 
 res = @timed est_pi(5000000000)
