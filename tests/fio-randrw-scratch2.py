7c826faf90dbd10065eddf14f8c203a765221af1
Derek Ryan Strong <dstrong@usc.edu>
2026-07-08 17:55:36 -0700

    Add fio-randrw-scratch2.py

---
+++ tests/fio-randrw-scratch2.py
@@ -0,0 +1,56 @@
+# Fio random read/write benchmark test
+# For /scratch2 file system
+# Purpose of test
+# - Test fio module access
+# - Test /scratch2 file system access
+# - Test /scratch2 file system performance
+# Notes
+# - https://github.com/axboe/fio
+# - Other I/O tests for this file system should not be run at the same time
+#   because performance results will be negatively impacted
+
+import reframe as rfm
+import reframe.utility.sanity as sn
+
+@rfm.simple_test
+class fio_randrw_scratch2(rfm.RunOnlyRegressionTest):
+    descr = "Fio random read/write benchmark for /scratch2 file system"
+    tags = {
+        "maintenance",
+        "performance",
+        "singlenode"
+    }
+    valid_systems = [
+        "discovery:epyc-7513",
+        "endeavour:epyc-7513"
+    ]
+    valid_prog_environs = [
+        "env-fio"
+    ]
+    sourcesdir = "src/fio-randrw"
+    executable = "bash fio-randrw.sh scratch2"
+    num_tasks = 1
+    num_cpus_per_task = 8
+    time_limit = "10m"
+    reference = {
+        "discovery:epyc-7513": {
+            "avg_write_speed": (30, -0.1, None, "MiB/sec"),
+            "avg_read_speed": (30, -0.1, None, "MiB/sec")
+        },
+        "endeavour:epyc-7513": {
+            "avg_write_speed": (30, -0.1, None, "MiB/sec"),
+            "avg_read_speed": (30, -0.1, None, "MiB/sec")
+        }
+    }
+
+    @sanity_function
+    def assert_sanity(self):
+        return sn.assert_found(r"all jobs", self.stdout)
+
+    @performance_function("MiB/sec", perf_key = "avg_write_speed")
+    def extract_perf_write(self):
+        return sn.extractsingle(r"WRITE:\sbw=(?P<W_ret>\d+.\d+)", self.stdout, "W_ret", float)
+
+    @performance_function("MiB/sec", perf_key = "avg_read_speed")
+    def extract_perf_read(self):
+        return sn.extractsingle(r"READ:\sbw=(?P<R_ret>\d+.\d+)", self.stdout, "R_ret", float)
