b7db9ec07d36cc1dcd375659a10bdf2663486058
Derek Ryan Strong <dstrong@usc.edu>
2026-07-31 14:45:28 -0700

    Update maintenance.sh

--- scripts/maintenance.sh
+++ scripts/maintenance.sh
@@ -19,10 +19,11 @@ tmux new-session -d -s rfm-apptainer-hello "reframe -c tests/apptainer-hello.py 
 tmux new-session -d -s rfm-apptainer-gpu-hello "reframe -c tests/apptainer-gpu-hello.py -J reservation=$res --distribute=all -r; bash"
 
 # Run NPB OMP MG test on all nodes
-tmux new-session -d -s rfm-npb-omp-mg "reframe -c tests/npb-omp-mg.py -p env-gcc-14.3.0 -J reservation=$res --distribute=all -r; bash"
+tmux new-session -d -s rfm-npb-omp-mg "reframe -c tests/npb-omp-mg.py -p gcc-15.3.0 -J reservation=$res --distribute=all -r; bash"
 
 # Run NPB CUDA LU test on all GPU nodes
-tmux new-session -d -s rfm-npb-cuda-lu "reframe -c tests/npb-cuda-lu.py -p env-gcc-14.3.0-cuda-12.9.1 -J reservation=$res --distribute=all -r; bash"
+tmux new-session -d -s rfm-npb-cuda-lu-cuda13 "reframe -c tests/npb-cuda-lu-cuda13.py -p gcc-15.3.0-cuda-13.0.3 -J reservation=$res --distribute=all -r; bash"
+tmux new-session -d -s rfm-npb-cuda-lu-cuda12 "reframe -c tests/npb-cuda-lu-cuda12.py -p gcc-14.3.0-cuda-12.9.1 -J reservation=$res --distribute=all -r; bash"
 
 # Run OMB bandwidth and latency tests on all nodes
 tmux new-session -d -s rfm-omb-bw "reframe -c tests/omb-bw.py -J reservation=$res --distribute=all -r; bash"
