5a4f59291790d179a254182b0b5f966ccbd0d7bd
Derek Ryan Strong <dstrong@usc.edu>
2026-07-31 16:13:08 -0700

    Update npb-mpi-mg.py

--- tests/npb-mpi-mg.py
+++ tests/npb-mpi-mg.py
@@ -26,12 +26,12 @@ class npb_mpi_mg(rfm.RunOnlyRegressionTest):
         "laguna:epyc-9554"
     ]
     valid_prog_environs = [
-        "gcc-15.3.0-openmpi-5.0.10",
         "gcc-15.3.0-mpich-5.0.1",
-        "gcc-14.3.0-openmpi-5.0.8",
+        "gcc-15.3.0-openmpi-5.0.10",
         "gcc-14.3.0-mpich-4.3.1",
-        "gcc-13.3.0-openmpi-5.0.5",
-        "gcc-13.3.0-mpich-4.2.2"
+        "gcc-14.3.0-openmpi-5.0.8",
+        "gcc-13.3.0-mpich-4.2.2",
+        "gcc-13.3.0-openmpi-5.0.5"
     ]
     sourcesdir = "src/npb-mpi"
     executable = "mg.D.x"
